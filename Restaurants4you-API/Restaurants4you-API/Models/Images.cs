823b910a40c4274d646fdb2d395442175c7b0418
Fernando Diogo <aluno22111@ipt.pt>
2022-12-01 15:51:47 +0000

    Classe Images

---
+++ Restaurants4you-API/Restaurants4you-API/Models/Images.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restaurant4you_API.Models
+{
+    /// <summary>
+    /// Descreve os dados das imagens
+    /// </summary>
+    public class Images
+    {
+        public Images() { }
+
+        /// <summary>
+        /// PK da tabela dos utilizadores
+        /// </summary>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// Caminho das imagens
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// FK para referenciar os restaurantes registados
+        /// </summary>
+        [ForeignKey(nameof(Restaurant))]
+        public int RestaurantFK { get; set; }
+        public Restaurants Restaurant { get; set; }
+    }
+}
