c7b8c310c7024b63244b1ead0b1e847639d12fe2
vasco2001 <aluno23055@ipt.pt>
2022-12-01 15:49:01 +0000

    Primeiro Commit

---
+++ Restaurants4you-API/Restaurants4you-API/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Restaurants4you_API.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
