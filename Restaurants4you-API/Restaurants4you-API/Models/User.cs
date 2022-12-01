8dc5cf9685cef6d2fecf0be7fcafcf7facfc88cb
vasco2001 <aluno23055@ipt.pt>
2022-12-01 15:52:33 +0000

    Adicionar as classes dos users

---
+++ Restaurants4you-API/Restaurants4you-API/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtWebApiTutorial
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
+    }
+}
